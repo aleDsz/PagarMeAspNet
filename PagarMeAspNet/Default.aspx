fc9880f019d04bba8043a9e507f55e3aad6dfa7f
Alexandre de Souza <xadee.13@gmail.com>
2016-07-24 02:57:27 -0300

    Init project

---
+++ PagarMeAspNet/Default.aspx
@@ -0,0 +1,15 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="PagarMeAspNet.Default" %>
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form action="ValidateRequest.aspx" method="post">
+        <input name="current_status" value="paid" /><br />
+        <input name="transaction[id]" value="123456789" /><br />
+        <input name="transaction[boleto_url]" value="http://pagar.me" /><br />
+        <input type="submit" value="Enviar" />
+    </form>
+</body>
+</html>
---
+++ PagarMeAspNet/ValidateRequest.aspx.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.IO;
+using PagarMe;
+
+namespace PagarMeAspNet
+{
+    public partial class ValidateRequest : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            PagarMeService.DefaultApiKey = "ak_test_qtDOZfF5K0VDn17k04NxnQPIZ3r5wV";
+
+            var postback_body = "" + Request.Form.ToString();
+            var signature = "" + Request.Headers["X-Hub-Signature"];
+            var fileName = Directory.GetCurrentDirectory() + "\\Log.txt";
+            var file = new FileStream(fileName, FileMode.OpenOrCreate, FileAccess.ReadWrite, FileShare.ReadWrite, 4096, true);
+            var Log = new StreamWriter(file);
+
+            Log.WriteLine("***********************************************");
+            if (Utils.ValidateRequestSignature(postback_body, signature))
+            {
+                var current_status = Request.Form["current_status"];
+                var transaction_id = Request.Form["transaction[id]"];
+                var boleto_url = Request.Form["transaction[boleto_url]"];
+
+                Log.WriteLine(String.Format("Valid Signature ({0})", signature));
+                Log.WriteLine(String.Format("Transaction ID: {0}", transaction_id));
+                Log.WriteLine(String.Format("Current Status: {0}", current_status));
+                Log.WriteLine(String.Format("    Boleto URL: {0}", boleto_url));
+            }
+            else
+                Log.WriteLine(String.Format("Invalid Signature ({0})", signature));
+            Log.WriteLine("***********************************************");
+
+            Log.Close();
+        }
+    }
+}
